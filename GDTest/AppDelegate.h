3c4a07340730660d5c7ae0ee1cd18eba61c6c4ea
qiuYangHANABI <qiuyang_hanabi@163.com>
2018-06-29 15:09:12 +0800

    Initial Commit

---
+++ GDTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDTest
+//
+//  Created by 黃秋陽 on 2018/6/29.
+//  Copyright © 2018年 黃秋陽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GDTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GDTest
+//
+//  Created by 黃秋陽 on 2018/6/29.
+//  Copyright © 2018年 黃秋陽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
